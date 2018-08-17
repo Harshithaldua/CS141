4b470a9734095422c54ed04f36b1139ae973279f
Harshithaldua <42206417+Harshithaldua@users.noreply.github.com>
2018-08-17 02:08:49 -0400

    Add files via upload

---
+++ thirdweek2.cpp
@@ -0,0 +1,33 @@
+// include library
+# include <iostream>
+using namespace std;
+
+int main(){
+float a,b, result;
+a=48;
+b=5;
+result =a/b;
+cout << result  << endl;
+int c,d,result1;
+c=54;
+d=21;
+result1 =c+d;
+cout << result1 << endl;
+int a1,b1,result2;
+a1=69;
+b1=96;
+result2 =a1-b1;
+cout << result2 <<endl;
+int c1,d1,result3;
+c1=21;
+d1=24;
+result3 =c1*d1;
+cout << result3 << endl;
+int a2,b2,result4;
+a2=1.5;
+b2=8.4;
+result4=a2%b2;
+cout << result4  << endl;
+return 0;
+}
+
