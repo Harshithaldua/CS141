63af4c7559b9421ef9583782d31105808358dd69
Harshithaldua <42206417+Harshithaldua@users.noreply.github.com>
2018-08-17 01:35:11 -0400

    Add files via upload

---
+++ thirdweek.cpp
@@ -0,0 +1,19 @@
+// included library
+#include <iostream>
+using namespace std;
+//mainfunction
+int main()
+{
+//declaring values
+int a=6;
+char b=69;
+float c=1.69;
+double d=2.015;
+bool e=6;
+//size
+cout<<sizeof(a)<<endl;
+cout<<sizeof(b)<<endl;
+cout<<sizeof(c)<<endl;
+cout<<sizeof(d)<<endl;
+cout<<sizeof(e)<<endl;
+}
